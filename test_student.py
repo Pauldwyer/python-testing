65af737001c9eafdae39753d7a18777438b9ca67
Pauldwyer <pauldwyer55@gmail.com>
2023-01-06 11:58:11 +0000

    finished mocking + module

--- test_student.py
+++ test_student.py
@@ -1,8 +1,20 @@
 import unittest
 from student import Student
+from datetime import timedelta
+from unittest.mock import patch
 
 
 class TestStudent(unittest.TestCase):
+    """
+    * Create a new test method called test_apply_extension
+    * Inside test_apply_extension, store the current end_date for our student instance in a variable called old_end_date
+    * Call a mthod named apply_extension that will take a number of days as an argument on the student instance to update the end_date
+    * Assert whetehr the instances' end_date eequals the old end date plus the days argument that was passed in using time delta
+    * Run the tests to confirm that the new mthod is failing
+    * In the Student Class, create a new method called apply_extension that has a parameter called ays
+    * Use the time delta method from date time to update the end_date property
+    * Run the tests to confirm they are working.
+    """
 
     @classmethod
     def setUpClass(cls):
@@ -19,6 +31,12 @@ class TestStudent(unittest.TestCase):
     def tearDown(self):
         print('tearDown')
 
+    def test_apply_extension(self):
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
+
     def test_full_name(self):
         print('test_full_name')
         self.assertEqual(self.student.full_name, 'John Doe')
@@ -32,6 +50,21 @@ class TestStudent(unittest.TestCase):
         self.student.alert_santa()
 
         self.assertTrue(self.student.naughty_list)
+    
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
+
+    def test_course_schedule_failure(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+            
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong with the request!")
 
 
 if __name__ == '__main__':
